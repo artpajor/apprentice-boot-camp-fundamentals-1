7f654c03096610d684f40bfe586a2a9fa6aa8c11
Art <adrasi@gmail.com>
2021-11-16 09:46:52 +0000

    final stage for today - deck class not being in use yet

---
+++ exercises/java/src/main/java/cards/Deck.java
@@ -0,0 +1,11 @@
+package cards;
+
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<String> cards = new ArrayList<>();
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+}
---
+++ exercises/java/src/main/java/cards/Suit.java
@@ -0,0 +1,13 @@
+package cards;
+
+public class Suit {
+    private String name;
+
+    public Suit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
